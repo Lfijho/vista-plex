48705497e42ab337696b156c8e2584841501e5eb
Lfijho <leandro.pinzon.filho@gmail.com>
2025-09-05 11:33:18 -0300

    Proxy Vercel

--- api/portainer/[[...path]].js
+++ api/portainer/[[...path]].js
@@ -6,14 +6,20 @@ export default async function handler(request, response) {
         const apiResponse = await fetch(targetUrl, {
             headers: {
                 'X-API-Key': 'ptr_82jZQyXfWd24T+cR0IgwS9JP503IN8GhO5+KO0lzIEc=',
-                'Content-Type': 'application/json',
             },
         });
 
+        if (!apiResponse.ok) {
+            const errorText = await apiResponse.text();
+            console.error(`Erro da API Portainer: ${apiResponse.status}`, errorText);
+            return response.status(apiResponse.status).json({ error: `Erro no servidor Portainer: ${apiResponse.statusText}` });
+        }
+
         const data = await apiResponse.json();
         response.status(200).json(data);
+
     } catch (error) {
-        console.error('Erro no proxy do Portainer:', error);
-        response.status(500).json({ error: 'Falha ao buscar dados do Portainer.' });
+        console.error('Erro no proxy do Portainer:', error.message);
+        response.status(500).json({ error: 'Falha interna ao tentar conectar com o Portainer.' });
     }
 }
